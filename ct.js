f2b54725a0cbafc7e8715e19df2303c464f7a0e9
mcrlmrkt <mia.marukot@mail.utoronto.ca>
2015-07-03 23:42:31 -0400

    Layout
    Committing layout of site

---
+++ ct.js
@@ -0,0 +1,14 @@
+function signup() {
+    document.getElementById("home_login").style.display = "none";
+    document.getElementById("home_signup").style.display = "block";
+}
+function profile() {
+    document.getElementById("news_feed").style.display = "none";
+    document.getElementById("courses").style.display = "none";
+    document.getElementById("wall").style.display = "inline-block";
+}
+function courses() {
+    document.getElementById("news_feed").style.display = "none";
+    document.getElementById("wall").style.display = "none";
+    document.getElementById("courses").style.display = "inline-block";
+}
