9ca6193794c4edbae2f3f28355df18eccf880839
mcrlmrkt <mia.marukot@mail.utoronto.ca>
2015-07-24 20:59:47 -0400

    server stuff

--- public/javascripts/ct.js
+++ public/javascripts/ct.js
@@ -17,3 +17,23 @@ function home() {
     document.getElementById("wall").style.display = "none";
     document.getElementById("courses").style.display = "none";
 }
+
+function previewFile(){
+       var preview = document.querySelector('dp'); //selects the query named img
+       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+       var reader  = new FileReader();
+
+       reader.onloadend = function () {
+           preview.src = reader.result;
+       }
+
+       if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+       } else {
+           preview.src = "";
+       }
+}
+
+function signout(){
+  document.location.href = "home.html";
+}
